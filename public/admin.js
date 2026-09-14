48934dd6e5c4370f87fb7a79771e9bcdde4d4ea5
Ali Nazer <roboatash2015@gmail.com>
2026-09-14 17:14:23 +0330

    Merged Setup & Admin pages
    Closes #4

--- public/admin.js
+++ public/admin.js
@@ -1,7 +1,24 @@
 const socket = io();
 
 const winby = { bo5: "3", bo3: "2", bo1: "1" };
+const applyables = [ "logoL", "logoR", "abbrL", "abbrR", "nameL", "nameR", "event_name", "event_stage"];
+
 let localstate = {};
+
+function setText(el, value) {
+    if (el instanceof HTMLInputElement || el instanceof HTMLTextAreaElement) {
+        el.value = value;
+    } else {
+        el.innerText = value;
+    }
+}
+function setImg(el, value) {
+    if (el instanceof HTMLInputElement || el instanceof HTMLTextAreaElement) {
+        el.value = value;
+    } else {
+        el.style.content = `url("${value}")`;
+    }
+}
 
 // Update admin panel UI when state changes
 function updateAdminUI(state) {
@@ -11,8 +28,12 @@ function updateAdminUI(state) {
             " - TIMEOUT ACTIVE: " +
             state["name" + state.timeout.team].toUpperCase();
     }
-    document.getElementById("matchtitle").innerText =
-        state.event_name + " - " + state.event_stage;
+    document.querySelectorAll(".eventname").forEach((el) => {
+        setText(el, state.event_name);
+    });
+    document.querySelectorAll(".eventstage").forEach((el) => {
+        setText(el, state.event_stage);
+    });
     document.getElementById("map").innerText =
         "Map " +
         state.map +
@@ -22,19 +43,31 @@ function updateAdminUI(state) {
         state.half +
         tm;
 
-    document.getElementById("tnameL").innerText = state.nameL.toUpperCase();
-    document.getElementById("abbrL").innerText = state.abbrL;
-    document.getElementById("logoL").style = `content: url("${state.logoL}")`;
     // Left LLLLLLLLLLLLLL
+    document.querySelectorAll(".tnameL").forEach((el) => {
+        setText(el, state.nameL);
+    });
+    document.querySelectorAll(".abbrL").forEach((el) => {
+        setText(el, state.abbrL.toUpperCase());
+    });
+    document.querySelectorAll(".logoL").forEach((el) => {
+        setImg(el, state.logoL);
+    });
     document.getElementById("scoreL").innerText =
         "■".repeat(state.scoreL) +
         "□".repeat(winby[state.format] - state.scoreL);
     document.getElementById("remL").innerText = state.timeout.L_remain;
 
-    document.getElementById("tnameR").innerText = state.nameR.toUpperCase();
-    document.getElementById("abbrR").innerText = state.abbrR;
-    document.getElementById("logoR").style = `content: url("${state.logoR}")`;
     // Right RRRRRRRRRRRRR
+    document.querySelectorAll(".tnameR").forEach((el) => {
+        setText(el, state.nameR);
+    });
+    document.querySelectorAll(".abbrR").forEach((el) => {
+        setText(el, state.abbrR.toUpperCase());
+    });
+    document.querySelectorAll(".logoR").forEach((el) => {
+        setImg(el, state.logoR);
+    });
     document.getElementById("scoreR").innerText =
         "■".repeat(state.scoreR) +
         "□".repeat(winby[state.format] - state.scoreR);
@@ -47,6 +80,24 @@ function updateAdminUI(state) {
         document.getElementById("tl").classList = "t";
         document.getElementById("tr").classList = "ct";
     }
+}
+
+function applySettings() {
+    for (const i of applyables) {
+        socket.emit("admin-command", {
+            action: "update",
+            target: i,
+            value: document.querySelector(`#${i}`).value
+        });
+    }
+    Edit0();
+}
+
+function Edit1() {
+    document.querySelector('body').classList = 'editmode';
+}
+function Edit0() {
+    document.querySelector('body').classList = 'noteditmode';
 }
 
 // Initialize the admin panel with the current state
